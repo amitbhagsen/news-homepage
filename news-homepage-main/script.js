fec06adcc0d0bf353076d0e4fa9c0ea878bd367e
amitbhagsen <109428244+amitbhagsen@users.noreply.github.com>
2024-04-01 17:13:25 +0530

    Add files via upload

---
+++ news-homepage-main/script.js
@@ -0,0 +1,18 @@
+const links = document.querySelector(".links");
+const menuOpen = document.querySelector(".menu-open");
+const menuClose = document.querySelector(".menu-close");
+const backDrop = document.querySelector(".backDrop");
+
+menuOpen.addEventListener("click", () => {
+  links.classList.add("active");
+  menuOpen.classList.add("active");
+  menuClose.classList.add("active");
+  backDrop.classList.add("active");
+});
+
+menuClose.addEventListener("click", () => {
+  links.classList.remove("active");
+  menuOpen.classList.remove("active");
+  menuClose.classList.remove("active");
+  backDrop.classList.remove("active");
+});
